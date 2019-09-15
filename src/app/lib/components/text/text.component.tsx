1f35c7a78451857cb62b489d7659fdd830e6f85e
Kacy Sommers <kacysommers@kacys-mbp.caketopia.lan>
2019-09-14 18:27:15 -0700

    adding text component

---
+++ src/app/lib/components/text/text.component.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './text.module.scss';
+import { Colors } from 'app/core/Colors.enum';
+
+type TextProps = {
+  content: string;
+  color?: string;
+  size?: string;
+}
+
+const getColor = (colorProp: string): Colors | string => {
+  if (colorProp) {
+    return Colors[colorProp] ? Colors[colorProp] : colorProp;
+  }
+  return Colors.$white;
+};
+
+export const TextComponent: React.FC<TextProps> = ({ content, color = '$white', size = '1rem' }) => {
+  return (
+    <p
+      className={styles.textContent}
+      style={{ color: getColor(color), fontSize: size }}>
+      {content}
+    </p>
+  );
+}
