18a417c9e85e2aefe27927f9146736805c6c516e
Kacy Sommers <kacysommers@android-331c3b5f7203a8a9.localdomain>
2019-09-15 17:53:11 -0700

    adding scroll behavior to home menu

--- src/app/pages/home/Home.page.tsx
+++ src/app/pages/home/Home.page.tsx
@@ -8,6 +8,8 @@ import { Colors } from 'app/core/Colors.enum';
 import { SliderComponent } from 'app/lib/components/slider/slider.component';
 import { toolIcons } from 'app/core/toolIcons';
 import LazyLoadedImage from 'app/lib/components/lazy-loaded-image/lazy-loaded-image.component';
+import { FooterComponent } from 'app/lib/components/footer/footer.component';
+import { HeaderComponent } from 'app/lib/components/header/header.component';
 
 const toolImages = toolIcons.map((tool: string) => (
   <div key={Math.floor(Math.random() * 10000)} className={styles.toolIconContainer}>
@@ -18,65 +20,80 @@ const toolImages = toolIcons.map((tool: string) => (
   </div>
 ));
 
-const Home: React.FC = () => {
-  return (
-    <div className={styles.homeContainer}>
-      <header className={styles.appHeader}>
-        <div className={styles.appHeaderLogoContainer}>
-          <span className={styles.logoSpan}>
-            <TextComponent content={'MKS'}></TextComponent>
-          </span>
-        </div>
-        <nav className={styles.appHeaderMenuContainer}>
-          <a className={styles.menuLink} href="#"><TextComponent content={'About'}></TextComponent></a>
-          <a className={styles.menuLink} href="#"><TextComponent content={'Projects'}></TextComponent></a>
-          <a className={styles.menuLink} href="#"><TextComponent content={'Resume'}></TextComponent></a>
-          <a className={styles.menuLink} href="#"><TextComponent content={'Contact'}></TextComponent></a>
-        </nav>
-      </header>
+export class Home extends React.Component<{}, {}> {
+  private projectsScrollMark: React.RefObject<HTMLDivElement>;
+  private aboutScrollMark: React.RefObject<HTMLDivElement>;
+  constructor(props: {}) {
+    super(props);
+    this.projectsScrollMark = React.createRef();
+    this.aboutScrollMark = React.createRef();
+  }
+  public scrollTo(event: React.MouseEvent, section: string) {
+    event.preventDefault();
+    this[`${section}ScrollMark`].current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  }
+  render() {
+    return (
+      <div className={styles.homeContainer}>
+        <header className={styles.appHeader}>
+          <HeaderComponent scrollTo={this.scrollTo.bind(this)}></HeaderComponent>
+        </header>
 
-      <section className={styles.landingContainer}>
-        <div className={styles.faceContainer}>
-          <img src={Face} alt="M Kacy Sommers Face" />
-        </div>
-        <div className={styles.ctaContainer}>
-          <CtaComponent title={'M Kacy Sommers'} body={'Full Stack Web Developer'} buttonText={'See Projects'}></CtaComponent>
-        </div>
-      </section>
+        <section className={styles.landingContainer}>
+          <div className={styles.faceContainer}>
+            <img src={Face} alt="M Kacy Sommers Face" />
+          </div>
+          <div className={styles.ctaContainer}>
+            <CtaComponent title={'M Kacy Sommers'} body={'Full Stack Web Developer'} buttonText={'See Projects'}></CtaComponent>
+          </div>
+        </section>
 
-      <section className={styles.aboutContainer}>
-        <div className={styles.aboutPhotoContainer}>
-          <img className={styles.aboutPhoto} src={LookLeft} alt="Kacy Looking Left" />
-        </div>
-        <div className={styles.aboutBioContainer}>
-          <TextComponent color={Colors.$background} content={'Hi, I\'m Kacy'}></TextComponent>
-          <TextComponent color={Colors.$background} content={'I\'m a Seattle based developer who loves to create full stack, fully responsive web applications that focus strongly on user experience.'}></TextComponent>
-          <TextComponent color={Colors.$background} content={'With a diverse background in education, leadership roles, creative work and customer service I have cultivated the logical and creative skills necessary to bring ideas to life online. Using technologies such as Node, React, Django, Postgres and MongoDB, I have written a variety of unique and helpful applications. I keep a curious and an open mind, and I\'m learning more every day.'}></TextComponent>
-          <TextComponent color={Colors.$background} content={'When I\'m not coding, I\'m usually walking my dog Joni and thinking about coding.'}></TextComponent>
-        </div>
-      </section>
+        <section className={styles.aboutContainer}>
+          <div ref={this.aboutScrollMark} className={styles.projectsScrollMark}></div>
+          <div className={styles.aboutPhotoContainer}>
+            <img className={styles.aboutPhoto} src={LookLeft} alt="Kacy Looking Left" />
+          </div>
+          <div className={styles.aboutBioContainer}>
+            <TextComponent color={Colors.$background} content={'Hi, I\'m Kacy'}></TextComponent>
+            <TextComponent color={Colors.$background} content={'I\'m a Seattle based developer who loves to create full stack, fully responsive web applications that focus strongly on user experience.'}></TextComponent>
+            <TextComponent color={Colors.$background} content={'With a diverse background in education, leadership roles, creative work and customer service I have cultivated the logical and creative skills necessary to bring ideas to life online. Using technologies such as Node, React, Django, Postgres and MongoDB, I have written a variety of unique and helpful applications. I keep a curious and an open mind, and I\'m learning more every day.'}></TextComponent>
+            <TextComponent color={Colors.$background} content={'When I\'m not coding, I\'m usually walking my dog Joni and thinking about coding.'}></TextComponent>
+          </div>
+        </section>
 
-      <section className={styles.projectsContainer}>
-        <div className={styles.sectionHeaderContainer}>
-          <TextComponent color={'$background'} content={'Projects'}></TextComponent>
-        </div>
-        <SliderComponent></SliderComponent>
-      </section>
+        <section
+          id="projects"
+          className={styles.projectsContainer}
+        >
+          <div ref={this.projectsScrollMark} className={styles.projectsScrollMark}></div>
+          <div className={styles.sectionHeaderContainer}>
+            <TextComponent color={'$background'} content={'Projects'}></TextComponent>
+          </div>
+          <SliderComponent></SliderComponent>
+        </section>
 
-      <section className={styles.skillsContainer}>
-        <div className={styles.sectionHeaderContainer}>
-          <TextComponent color={'$primary'} content={'Skills'}></TextComponent>
-        </div>
-        <div className={styles.toolImagesContainer}>
-          {toolImages}
-        </div>
-      </section>
+        <section className={styles.skillsContainer}>
+          <div className={styles.sectionHeaderContainer}>
+            <TextComponent color={'$primary'} content={'Skills'}></TextComponent>
+          </div>
+          <div className={styles.toolImagesContainer}>
+            {toolImages}
+          </div>
+        </section>
 
-      <section className={styles.getInTouchContainer}>
-        <CtaComponent title={'Get in touch!'} body={'I am always on the lookout for new projects, fresh challenges and kind folks to collaborate with. If you have an idea, an open position or just want to talk code, please get in touch.'} buttonText={'Contact Kacy'}></CtaComponent>
-      </section>
-    </div>
-  );
+        <section className={styles.getInTouchContainer}>
+          <CtaComponent title={'Get in touch!'} body={'I am always on the lookout for new projects, fresh challenges and kind folks to collaborate with. If you have an idea, an open position or just want to talk code, please get in touch.'} buttonText={'Contact Kacy'}></CtaComponent>
+        </section>
+
+        <section className={styles.footerContainer}>
+          <FooterComponent></FooterComponent>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Home;
