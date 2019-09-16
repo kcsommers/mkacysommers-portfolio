c11735ebe750b9bfceb6171fcd472c1e18b7d528
Kacy Sommers <kacysommers@Kacys-MacBook-Pro.local>
2019-09-15 19:36:11 -0700

    animating header on scroll

--- src/app/lib/components/header/header.component.tsx
+++ src/app/lib/components/header/header.component.tsx
@@ -10,43 +10,79 @@ type HeaderProps = {
   scrollTo: (event: React.MouseEvent, section: string) => void
 }
 
-export const HeaderComponent: React.FC<HeaderProps> = ({ scrollTo }) => {
-  return (
-    <div className={styles.headerContainer}>
-      <div className={styles.appHeaderLogoContainer}>
-        <span className={styles.logoSpan}>
-          <TextComponent content={'MKS'}></TextComponent>
-        </span>
+type HeaderState = {
+  scrolled: boolean;
+}
+
+export class HeaderComponent extends React.Component<HeaderProps, HeaderState> {
+  constructor(props: HeaderProps) {
+    super(props);
+    this.state = {
+      scrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  private handleScroll(event: Event) {
+    const { scrolled } = this.state;
+    if (window.scrollY > 30) {
+      if (!scrolled) {
+        this.setState({ scrolled: true });
+      }
+    } else {
+      if (scrolled) {
+        this.setState({ scrolled: false });
+      }
+    }
+  }
+
+  render() {
+    const { scrollTo } = this.props;
+    const { scrolled } = this.state;
+    return (
+      <div className={[styles.headerContainer, scrolled && styles.headerScrolled].join(' ')}>
+        <div className={styles.appHeaderLogoContainer}>
+          <span className={styles.logoSpan}>
+            <TextComponent content={'MKS'}></TextComponent>
+          </span>
+        </div>
+        <nav className={styles.appHeaderMenuContainer}>
+          <a
+            className={styles.menuLink}
+            href="#about"
+            onClick={(e) => { scrollTo(e, 'about') }}
+          ><TextComponent content={'About'}></TextComponent>
+          </a>
+          <a
+            className={styles.menuLink}
+            href="#projects"
+            onClick={(e) => { scrollTo(e, 'projects') }}
+          ><TextComponent content={'Projects'}></TextComponent>
+          </a>
+          <a
+            className={styles.menuLink}
+            href="#"
+          ><TextComponent content={'Resume'}></TextComponent>
+          </a>
+          <a
+            className={styles.menuLink}
+            href="#"
+          ><TextComponent content={'Contact'}></TextComponent>
+          </a>
+          <a
+            className={styles.menuLink}
+            href="#"
+          ><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+          </a>
+        </nav>
       </div>
-      <nav className={styles.appHeaderMenuContainer}>
-        <a
-          className={styles.menuLink}
-          href="#about"
-          onClick={(e) => { scrollTo(e, 'about') }}
-        ><TextComponent content={'About'}></TextComponent>
-        </a>
-        <a
-          className={styles.menuLink}
-          href="#projects"
-          onClick={(e) => { scrollTo(e, 'projects') }}
-        ><TextComponent content={'Projects'}></TextComponent>
-        </a>
-        <a
-          className={styles.menuLink}
-          href="#"
-        ><TextComponent content={'Resume'}></TextComponent>
-        </a>
-        <a
-          className={styles.menuLink}
-          href="#"
-        ><TextComponent content={'Contact'}></TextComponent>
-        </a>
-        <a
-          className={styles.menuLink}
-          href="#"
-        ><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
-        </a>
-      </nav>
-    </div>
-  );
+    );
+  }
 }
