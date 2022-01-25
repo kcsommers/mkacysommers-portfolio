eb0a79efe74735f071218114b84a1bbcd7c98da8
Kacy Sommers <kacysommers@gmail.com>
2022-01-25 10:52:23 -0800

    revert base view changes

--- src/app/views/Home/Home.tsx
+++ src/app/views/Home/Home.tsx
@@ -1,25 +1,23 @@
 import { FC } from 'react';
 import { Nav } from '../../components';
-import { BaseView } from '../BaseView';
 import styles from './Home.module.scss';
 
 export const Home: FC = () => {
-  const leftView = (
-    <>
-      <h3>M Kacy Sommers</h3>
-      <p className={styles.blurb}>Web Developer. Javascript Professional.</p>
-      <Nav />
-    </>
-  );
-
-  const rightView = (
-    <div className={styles.logoWrap}>
-      <img
-        src="https://res.cloudinary.com/kcsommers/image/upload/v1643094250/mksommerslogo.png"
-        alt="M Kacy Sommers logo"
-      />
+  return (
+    <div className={styles.homeWrap}>
+      <div className={styles.homeIntroWrap}>
+        <h3>M Kacy Sommers</h3>
+        <p>Web Developer. Javascript Professional.</p>
+        <Nav />
+      </div>
+      <div className={styles.homeRight}>
+        <div className={styles.logoWrap}>
+          <img
+            src="https://res.cloudinary.com/kcsommers/image/upload/v1643094250/mksommerslogo.png"
+            alt="M Kacy Sommers logo"
+          />
+        </div>
+      </div>
     </div>
   );
-
-  return <BaseView left={leftView} right={rightView} />;
 };
