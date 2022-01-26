b13960e0322a632c57a69efa6aa0c97fe2e88599
Kacy Sommers <kacysommers@gmail.com>
2022-01-26 11:46:06 -0800

    reorganize core module

--- src/app/core/data/projects/project-cateogries.ts
+++
@@ -1,5 +0,0 @@
-export const ProjectCategories = {
-  FULL_TIME: 'Full Time',
-  FREELANCE: 'Freelance',
-  EARLY_DAYS: 'Early Days',
-};
--- src/app/core/public.ts
+++
@@ -1,7 +0,0 @@
-export * from './common/constants';
-export { useResize } from './common/hooks/use-resize';
-export { useDebounce } from './common/hooks/use-debounce';
-export type { IProject } from './data/projects/IProject';
-export { projects } from './data/projects/projects';
-export type { Page } from './page.type';
-export * from './common/utils';
