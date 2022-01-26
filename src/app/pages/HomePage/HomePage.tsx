e5eafc2f16f334f5b0f8b2097bdb790ef4527f69
Kacy Sommers <kacysommers@gmail.com>
2022-01-25 23:22:41 -0800

    remove old pages

--- src/app/pages/HomePage/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-import { PageWrap } from '@components';
-import { RouteComponentProps } from 'react-router';
-import styles from './HomePage.module.scss';
-
-interface HomePageProps extends RouteComponentProps {}
-
-export const HomePage = ({}: HomePageProps) => {
-  return <PageWrap page="home" />;
-};
--- src/app/pages/public.ts
+++
@@ -1,5 +0,0 @@
-export { HomePage } from './HomePage/HomePage';
-export { AboutPage } from './AboutPage/AboutPage';
-export { ContactPage } from './ContactPage/ContactPage';
-export { ProjectsPage } from './ProjectsPage/ProjectsPage';
-export { ResumePage } from './ResumePage/ResumePage';
